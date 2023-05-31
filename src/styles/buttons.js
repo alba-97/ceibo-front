5c94007f8f1db3e4532ae686fa689fcc98032b68
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-05-31 19:34:47 -0300

    created common button

---
+++ src/styles/buttons.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const genericButtonStyle = StyleSheet.create({
+    button: {
+      backgroundColor: 'transparent',
+      borderRadius: 25,
+      padding: 10,
+      alignItems: 'center',
+      marginTop: 10,
+      borderWidth: 1,
+      borderColor: "black"
+    },
+    buttonText: {
+      color: 'black',
+      fontSize: 16,
+    },
+});
