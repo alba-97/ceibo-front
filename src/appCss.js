f96b80e0f221b76be71873eafc1593f5dbe1893f
valentin <bellberger45@gmail.com>
2023-06-01 11:15:45 -0300

    removing obsolete styles

--- src/appCss.js
+++ src/appCss.js
@@ -3,21 +3,4 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  header: {
-    borderBottomWidth: 1,
-    borderBottomColor: "#fff",
-    width: "100%",
-    height: "13%",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    top: 0,
-  },
-  text: {
-    color: "#F6F3E4",
-    fontSize: 30,
-    textShadowColor: "#fd6115",
-    textShadowOffset: { width: 2, height: 2 },
-    textShadowRadius: 1,
-  },
 });
