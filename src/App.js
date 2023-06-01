5c6fe27640fda30ba0b8213ad8033d0380914735
leonstefano03 <leonstefano03@gmail.com>
2023-06-01 12:30:51 -0300

    bugs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "./appCss";
-// import { GenericInput } from './components/TextInput';
-// import { ImageContainer } from './components/ImageContainer';
-// import { TextDeUsuario } from './components/TextDeUsuario';
-// import { FotoDeUsuario } from './components/FotoDeUsuario';
-// import Calendar from './components/Calendar';
+import { GenericInput } from "./components/TextInput";
+import { ImageContainer } from "./components/ImageContainer";
+import { TextDeUsuario } from "./components/TextDeUsuario";
+import { FotoDeUsuario } from "./components/FotoDeUsuario";
+import { Navbar } from "./components/Navbar";
 export default function App() {
   return (
     <LinearGradient
@@ -17,27 +16,24 @@ export default function App() {
       end={[1, 1]}
       style={styles.container}
     >
-         <View style={styles.container}>
+    <View style={styles.container}>
+      <Navbar />
 
-<Navbar/>
-
-<ImageContainer
-imageSource="https://www.billboard.com/wp-content/uploads/2022/09/bad-bunny-press-credit-eric-rojas-2022-billboard-2-1548.jpg?w=942&h=623&crop=1"
-date="28 de mayo de 2023"
- event="Conejo malo"
+      <ImageContainer
+        imageSource="https://www.billboard.com/wp-content/uploads/2022/09/bad-bunny-press-credit-eric-rojas-2022-billboard-2-1548.jpg?w=942&h=623&crop=1"
+        date="28 de mayo de 2023"
+        event="Conejo malo"
       />
       <GenericInput
-        // otras propiedades
-
+      // otras propiedades
       />
 
       <TextDeUsuario text="Leon Stefano" />
 
-      <FotoDeUsuario imageSource="https://st2.depositphotos.com/1017732/9796/i/450/depositphotos_97968600-stock-photo-pensive-man-looking-at-the.jpg"/>
-  
-        <StatusBar style="auto" />
-      </View>
-    </LinearGradient>
+      <FotoDeUsuario imageSource="https://st2.depositphotos.com/1017732/9796/i/450/depositphotos_97968600-stock-photo-pensive-man-looking-at-the.jpg" />
 
+      <StatusBar style="auto" />
+    </View>
+     </LinearGradient>
   );
 }
