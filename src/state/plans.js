05be7e7ce45ec429c6e9ed05da15dc8e842cc704
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-06-07 19:12:05 -0300

    added changes to redux

--- src/state/plans.js
+++ src/state/plans.js
@@ -1,22 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  id: null,
-  title: null,
-  description: null,
-  img: null,
-  event_date: null,
-  created_at: null,
-  min_age: null,
-  max_age: null,
-  min_to_pay: null,
-  total_to_pay: null,
-  deadline_to_pay: null,
-  link_to_pay: null,
-  category: null,
-  start_time: null,
-  end_time: null,
-};
+const initialState = [];
 
 const plansSlice = createSlice({
   name: "plans",
