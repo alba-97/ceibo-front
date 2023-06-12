044b126b37537b4a4344dad0650b61ee4749b807
alba-97 <alvarezbasilio97@gmail.com>
2023-06-12 15:53:13 -0300

    planDetails responsive

--- src/screens/PlanDetailScreen.jsx
+++ src/screens/PlanDetailScreen.jsx
@@ -1,15 +1,9 @@
 import { PlanDetailCard } from "../components/PlanDetailCard";
 import { LinearGradient } from "expo-linear-gradient";
-import { styles } from "../appCss";
 
 export default function PlanDetailScreen() {
   return (
-    <LinearGradient
-      colors={["#000", "#7D0166"]}
-      start={[0, 0]}
-      end={[1, 1]}
-      style={styles.container}
-    >
+    <LinearGradient colors={["#000", "#7D0166"]} start={[0, 0]} end={[1, 1]}>
       <PlanDetailCard />
     </LinearGradient>
   );
