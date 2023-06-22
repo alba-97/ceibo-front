dc0906331fb8123ba2f896ebedb1ad18ac155981
valentin <bellberger45@gmail.com>
2023-06-22 16:47:08 -0300

    margin bottom

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -112,6 +112,7 @@ export default function HomeScreen() {
             )}
           </View>
         )}
+        <View style={{ marginBottom: 30 }}></View>
       </ScrollView>
     </LinearGradient>
   );
