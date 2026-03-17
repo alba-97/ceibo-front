149c960e0c6dbe2e6530e3c9f0b6a0ea1c5b3964
alba-97 <alvarezbasilio97@gmail.com>
2026-03-17 06:05:49 -0300

    fix regenerator-runtime error

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
@@ -8,6 +8,16 @@ config.transformer = {
   minifierConfig: {
     ...config.transformer?.minifierConfig,
     keep_fnames: true,
+  },
+};
+
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    "regenerator-runtime": path.resolve(
+      __dirname,
+      "node_modules/regenerator-runtime"
+    ),
   },
 };
 
