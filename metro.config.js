412d6403719787b8c389cbab80de08da4ec0c552
alba-97 <alvarezbasilio97@gmail.com>
2026-03-17 06:53:12 -0300

    upgrade dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,29 +1,3 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-const config = getDefaultConfig(__dirname);
-
-config.transformer = {
-  ...config.transformer,
-  minifierConfig: {
-    ...config.transformer?.minifierConfig,
-    keep_fnames: true,
-  },
-};
-
-const regeneratorRuntimePath = require.resolve("regenerator-runtime/runtime");
-
-config.resolver = {
-  ...config.resolver,
-  resolveRequest: (context, moduleName, platform) => {
-    if (
-      moduleName === "regenerator-runtime" ||
-      moduleName === "regenerator-runtime/runtime" ||
-      moduleName === "@babel/runtime/regenerator"
-    ) {
-      return { filePath: regeneratorRuntimePath, type: "sourceFile" };
-    }
-    return context.resolveRequest(context, moduleName, platform);
-  },
-};
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
