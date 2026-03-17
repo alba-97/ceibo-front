04ff121705947a2cbc7260594c9ad0e6b4be949b
alba-97 <alvarezbasilio97@gmail.com>
2026-03-17 05:44:31 -0300

    change terser config

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,14 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+config.transformer = {
+  ...config.transformer,
+  minifierConfig: {
+    ...config.transformer?.minifierConfig,
+    keep_fnames: true,
+  },
+};
+
+module.exports = config;
